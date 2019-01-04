1cc18679e6c9ffe04635b2edb6c01602ccbb863b
Johannes Kümmel <johannes.kummel@roguewave.com>
2019-01-04 17:52:17 +0200

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# esport-recritment
+
+Website for recruiting volunteers for TalTech e-Sport.
+
+### Requirements
++ Node.js
+
+### Development
+
+Run `npm install` to install packages.
+
+Run `npm run start` to start application in development mode on locahost:3000
+
+### Build
+
+Run `npm install` to install packages.
+
+Run `npm run build` to build website.
+
+Built files will be located in the `build` directory.
---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+/*global require*/
+"use strict";
+
+var gulp = require('gulp'),
+  path = require('path'),
+  pug = require('gulp-pug'),
+  prefix = require('gulp-autoprefixer'),
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync');
+
+/*
+ * Directories here
+ */
+var paths = {
+  source: './src/',
+  build: './build/',
+  sass: 'sass/',
+  css: 'css/',
+  images: 'images/'
+};
+
+gulp.task('pug', function () {
+  return gulp.src(paths.source + '*.pug')
+    .pipe(pug())
+    .on('error', function (err) {
+      process.stderr.write(err.message + '\n');
+      this.emit('end');
+    })
+    .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('positions-pug', function () {
+  return gulp.src(paths.source + 'positions/*.pug')
+    .pipe(pug())
+    .on('error', function (err) {
+      process.stderr.write(err.message + '\n');
+      this.emit('end');
+    })
+    .pipe(gulp.dest(paths.build + "positions/"));
+});
+
+gulp.task('images', function () {
+  return gulp.src(paths.source + paths.images + "**/*.*")
+    .pipe(gulp.dest(paths.build + paths.images))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+})
+
+gulp.task('rebuild', ['pug', 'positions-pug', 'sass', 'images'], function () {
+  browserSync.reload();
+});
+
+gulp.task('browser-sync', ['sass', 'pug', 'positions-pug', 'images'], function () {
+  browserSync({
+    server: {
+      baseDir: paths.build
+    },
+    notify: true
+  });
+});
+
+gulp.task('sass', function () {
+  return gulp.src(paths.source + paths.sass + 'style.scss')
+    .pipe(sass({
+      includePaths: [paths.source + paths.sass],
+      outputStyle: 'compressed'
+    }))
+    .on('error', sass.logError)
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+      cascade: true
+    }))
+    .pipe(gulp.dest(paths.build + paths.css))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(paths.source + paths.sass + '**/*.scss', ['sass']);
+  gulp.watch(paths.source + '**/*.pug', ['rebuild']);
+  gulp.watch(paths.source + paths.images + '**/*.*', ['images']);
+});
+
+gulp.task('build', ['sass', 'pug', 'positions-pug', 'images']);
+
+
+gulp.task('default', ['browser-sync', 'watch']);
